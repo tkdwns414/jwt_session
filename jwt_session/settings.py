47e032e7524406dd9b07886fc53e67d4d84ec424
tkdwns414 <sangjune2000@naver.com>
2022-07-10 15:10:08 +0900

    settings

--- jwt_session/settings.py
+++ jwt_session/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8#33g%g7s+(^kwi_*p)%)901yb^h7qdvbqs$v!^8zgk3gv0%h_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
